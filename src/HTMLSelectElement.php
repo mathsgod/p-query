b364346b4d5386966d119c3a1d3287d2b0cf6956
Raymond Chong <raymond@nas.hlhk.net>
2018-05-10 17:16:15 +0800

    HTMLSelectElement options update

--- src/HTMLSelectElement.php
+++ src/HTMLSelectElement.php
@@ -1,21 +1,24 @@
 <?php
-
 namespace P;
-class HTMLSelectElement extends HTMLElement {
+
+class HTMLSelectElement extends HTMLElement
+{
     public $required = false;
     public $name = null;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct("select");
     }
 
-    public function __set($name, $value) {
+    public function __set($name, $value)
+    {
         if ($name == "value") {
-        	$options=[];
-            foreach($this->childNodes as $node) {
-            	if($node instanceof HTMLOptionElement){
-            		$options[]=$nodes;
-            	}
+            $options = [];
+            foreach ($this->childNodes as $node) {
+                if ($node instanceof HTMLOptionElement) {
+                    $options[] = $nodes;
+                }
                 if ($node->attributes["value"] == $value) {
                     $node->selected = true;
                 } else {
@@ -29,12 +32,24 @@ class HTMLSelectElement extends HTMLElement {
         }
     }
 
-    public function add(HTMLOptionElement $item) {
+    public function options($arrs)
+    {
+        foreach ($arrs as $v) {
+            $item = new HTMLOptionElement($v, $v);
+            $this->add($item);
+        }
+        return;
+    }
+
+    public function add(HTMLOptionElement $item)
+    {
         $this->appendChild($item);
     }
-    public function __toString() {
-        if ($this->required)$this->attributes["request"] = $this->required;
-        if ($this->name)$this->attributes["name"] = $this->name;
+
+    public function __toString()
+    {
+        if ($this->required) $this->attributes["request"] = $this->required;
+        if ($this->name) $this->attributes["name"] = $this->name;
         /*        if ($this->value) {
             foreach($this->childNodes as $node) {
                 if ($node->value == $this->value) {
@@ -44,11 +59,12 @@ class HTMLSelectElement extends HTMLElement {
                 }
             }
         }
-*/
+         */
         return parent::__toString();
     }
-    
-	public function item($index){
-		return $this->getElementsByTagName("option")[$index];
-	}
+
+    public function item($index)
+    {
+        return $this->getElementsByTagName("option")[$index];
+    }
 }
