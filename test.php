18db0e29d91fe14d9d98dcfc2c2d24ce5e91533d
Raymond Chong <mathsgod@yahoo.com>
2019-05-07 17:25:42 +0800

    update

--- test.php
+++ test.php
@@ -1,28 +1,3 @@
 <?
 require_once("vendor/autoload.php");
 
-use P\HTMLOptionElement;
-
-$select = p("<select></select>")[0];
-
-
-foreach (range(1, 4) as $v) {
-
-    $opt = new HTMLOptionElement();
-    $opt->value = $v;
-    $opt->textContent = $v;
-    $select->add($opt);
-}
-print_r($select);
-
-foreach($select->childNodes as $node){
-    print_r($node);
-}
-die();
-
-print_r(p($select)->find("option"));
-return;
-foreach (p($select)->find("option") as $opt) {
-    print_r($opt);
-    print_r($opt->value);
-}
