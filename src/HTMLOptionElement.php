186cf9df772c02b74360859b70dd7817371d4731
Raymond Chong <mathsgod@yahoo.com>
2021-11-17 13:06:27 +0800

    option

--- src/HTMLOptionElement.php
+++ src/HTMLOptionElement.php
@@ -1,18 +1,74 @@
 <?php
+
 namespace P;
 
+/**
+ * @property bool $defaultSelected
+ * @property bool $disabled
+ * @property bool $selected
+ * @property string $text
+ * @property string $value
+ */
 class HTMLOptionElement extends HTMLElement
 {
-    const ATTRIBUTES = [
-        "defaultSelected" => "bool",
-        "disabled" => "bool",
-        "label" => "string",
-        "selected" => "bool",
-        "value" => "string"
-    ] + parent::ATTRIBUTES;
-
     public function __construct($value = "", $uri = null)
     {
         parent::__construct("option", $value, $uri);
+    }
+
+    function __get($name)
+    {
+        switch ($name) {
+            case "defaultSelected":
+                return $this->hasAttribute("defaultSelected");
+            case "disabled":
+                return $this->hasAttribute("disabled");
+            case "selected":
+                return $this->hasAttribute("selected");
+            case "text":
+                return $this->textContent;
+            case "value":
+                if (!$this->hasAttribute("value")) {
+                    return $this->getAttribute("text");
+                }
+                return $this->textContent;
+        }
+        return parent::__get($name);
+    }
+
+    function __set($name, $value)
+    {
+        switch ($name) {
+            case "defaultSelected":
+                if ($value) {
+                    $this->setAttribute("defaultSelected", true);
+                } else {
+                    $this->removeAttribute("defaultSelected");
+                }
+                break;
+            case "disabled":
+                if ($value) {
+                    $this->setAttribute("disabled", true);
+                } else {
+                    $this->removeAttribute("disabled");
+                }
+                break;
+            case "selected":
+                if ($value) {
+                    $this->setAttribute("selected", true);
+                } else {
+                    $this->removeAttribute("selected");
+                }
+                break;
+            case "text":
+                $this->setAttribute("text", $value);
+                break;
+            case "value":
+                $this->setAttribute("value", $value);
+                break;
+            default:
+                parent::__set($name, $value);
+                break;
+        }
     }
 }
