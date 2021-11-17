d1011c624b40d0cf22a13d0a0139683afab3d891
Raymond Chong <mathsgod@yahoo.com>
2021-11-17 12:30:53 +0800

    button

--- src/HTMLButtonElement.php
+++ src/HTMLButtonElement.php
@@ -2,10 +2,39 @@
 
 namespace P;
 
+/**
+ * @property bool $disabled
+ * @property string $name
+ */
+
 class HTMLButtonElement extends HTMLElement
 {
     public function __construct($value = "", $uri = null)
     {
         parent::__construct("button", $value, $uri);
+    }
+
+    function __set($name, $value)
+    {
+        switch ($name) {
+            case "disabled":
+                $this->setAttribute("disabled", $value);
+                return;
+            case "name":
+                $this->setAttribute("name", $value);
+                return;
+        }
+        parent::__set($name, $value);
+    }
+
+    function __get($name)
+    {
+        switch ($name) {
+            case "disabled":
+                return $this->getAttribute("disabled");
+            case "name":
+                return $this->getAttribute("name");
+        }
+        return parent::__get($name);
     }
 }
