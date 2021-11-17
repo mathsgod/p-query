8646987d8bb8f6e9cd160ee0abf58d224ce47dcb
Raymond Chong <mathsgod@yahoo.com>
2021-11-17 13:00:14 +0800

    label

--- src/HTMLInputElement.php
+++ src/HTMLInputElement.php
@@ -1,18 +1,21 @@
 <?php
+
 namespace P;
 
+/**
+ * @property string $name
+ * @property string $type
+ * @property bool $disabled
+ * @property bool $autofocus
+ * @property bool $required 
+ * @property string $value
+ */
 class  HTMLInputElement extends HTMLElement
 {
     const ATTRIBUTES = [
-        "name" => "string",
-        "required" => "bool",
         "checked" => "bool",
         "defaultChecked" => "bool",
         "indeterminate" => "bool",
-        "type" => "string",
-        "disabled" => "bool",
-        "autofocus" => "bool",
-        "value" => "string",
         "alt" => "string",
         "height" => "string",
         "src" => "string",
@@ -33,5 +36,64 @@ class  HTMLInputElement extends HTMLElement
     public function __construct($value = "", $uri = null)
     {
         parent::__construct("input", $value, $uri);
+    }
+
+    function __get($name)
+    {
+        switch ($name) {
+            case "name":
+                return $this->getAttribute("name");
+            case "type":
+                return $this->getAttribute("type");
+            case "disabled":
+                return $this->hasAttribute("disabled");
+            case "autofocus":
+                return $this->hasAttribute("autofocus");
+            case "required":
+                return $this->hasAttribute("required");
+            case "value":
+                return $this->getAttribute("value");
+            default:
+                return parent::__get($name);
+        }
+    }
+
+    function __set($name, $value)
+    {
+        switch ($name) {
+            case "name":
+                $this->setAttribute("name", $value);
+                break;
+            case "type":
+                $this->setAttribute("type", $value);
+                break;
+            case "disabled":
+                if ($value) {
+                    $this->setAttribute("disabled", true);
+                } else {
+                    $this->removeAttribute("disabled");
+                }
+                break;
+            case "autofocus":
+                if ($value) {
+                    $this->setAttribute("autofocus", true);
+                } else {
+                    $this->removeAttribute("autofocus");
+                }
+                break;
+            case "required":
+                if ($value) {
+                    $this->setAttribute("required", true);
+                } else {
+                    $this->removeAttribute("required");
+                }
+                break;
+            case "value":
+                $this->setAttribute("value", $value);
+                break;
+            default:
+                parent::__set($name, $value);
+                break;
+        }
     }
 }
