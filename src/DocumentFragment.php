d6c3e6acfe7771be75187f1047c36ecff203763a
Raymond Chong <mathsgod@yahoo.com>
2021-11-17 15:26:16 +0800

    document fregment

--- src/DocumentFragment.php
+++ src/DocumentFragment.php
@@ -1,7 +1,26 @@
 <?php
+
 namespace P;
 
 use DOMDocumentFragment;
 
+/**
+ * @property-read Element $firstElementChild
+ */
 class DocumentFragment extends DOMDocumentFragment
-{ }
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        Document::Current()->appendChild($this);
+    }
+
+    function __get($name)
+    {
+        switch ($name) {
+            case "firstElementChild":
+                return $this->firstChild;
+        }
+    }
+}
