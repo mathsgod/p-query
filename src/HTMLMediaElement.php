27a57b75bcb057f91b8c22c15838831172a9b076
Raymond Chong <mathsgod@yahoo.com>
2022-04-21 15:29:43 +0800

    media

--- src/HTMLMediaElement.php
+++ src/HTMLMediaElement.php
@@ -4,6 +4,7 @@ namespace P;
 
 /**
  * @property bool $autoplay A boolean value that reflects the autoplay HTML attribute, indicating whether playback should automatically begin as soon as enough media is available to do so without interruption.
+ * @property bool $controls A boolean value that reflects the controls HTML attribute, indicating whether the browser should show playback controls to the user.
  */
 class HTMLMediaElement extends HTMLElement
 {
@@ -17,6 +18,27 @@ class HTMLMediaElement extends HTMLElement
             }
         }
 
+        if ($name == "controls") {
+            if ($value) {
+                $this->setAttribute("controls", "");
+            } else {
+                $this->removeAttribute("controls");
+            }
+        }
+
         parent::__set($name, $value);
+    }
+
+    public function __get($name)
+    {
+        if ($name == "autoplay") {
+            return $this->hasAttribute("autoplay");
+        }
+
+        if ($name == "controls") {
+            return $this->hasAttribute("controls");
+        }
+
+        return parent::__get($name);
     }
 }
