538d9c293a789a1e8c5f654b1982a69ed5552373
Raymond Chong <mathsgod@yahoo.com>
2022-04-22 17:50:07 +0800

    update

--- src/HTMLButtonElement.php
+++ src/HTMLButtonElement.php
@@ -3,44 +3,67 @@
 namespace P;
 
 /**
- * @property bool $disabled
- * @property string $name
- * @property string $value
+ * @property bool $autofocus Is a boolean value indicating whether or not the control should have input focus when the page loads, unless the user overrides it, for example by typing in a different control. Only one form-associated element in a document can have this attribute specified.
+ * @property bool $disabled Is a boolean value indicating whether or not the control is disabled, meaning that it does not accept any clicks.
+ * @property string $name Is a DOMString representing the name of the object when submitted with a form. If specified, it must not be the empty string.
+ * @property string $type Is a DOMString representing the type of button. Possible values are: submit , reset , button , and menu .
+ * @property string $value Is a DOMString representing the current form control value of the button.
  */
 
 class HTMLButtonElement extends HTMLElement
 {
-    public function __construct($value = "", $uri = null)
+    public function __construct(string|null $value = null, string $namespace = null)
     {
-        parent::__construct("button", $value, $uri);
+        parent::__construct("button", $value, $namespace);
     }
 
-    function __set($name, $value)
+    public function __get($name)
     {
-        switch ($name) {
-            case "disabled":
-                $this->setAttribute("disabled", $value);
-                return;
-            case "name":
-                $this->setAttribute("name", $value);
-                return;
-            case "value":
-                $this->setAttribute("value", $value);
-                return;
+        if ($name == "autofocus") {
+            return $this->hasAttribute("autofocus");
         }
-        parent::__set($name, $value);
-    }
+        if ($name == "disabled") {
+            return $this->hasAttribute("disabled");
+        }
+        if ($name == "name") {
+            return $this->getAttribute("name");
+        }
+        if ($name == "type") {
+            return $this->getAttribute("type");
+        }
+        if ($name == "value") {
+            return $this->getAttribute("value");
+        }
 
-    function __get($name)
-    {
-        switch ($name) {
-            case "disabled":
-                return $this->getAttribute("disabled");
-            case "name":
-                return $this->getAttribute("name");
-            case "value":
-                return $this->getAttribute("value");
-        }
         return parent::__get($name);
+    }
+
+    public function __set($name, $value)
+    {
+        if ($name == "autofocus") {
+            if ($value) {
+                $this->setAttribute("autofocus", "");
+            } else {
+                $this->removeAttribute("autofocus");
+            }
+        }
+        if ($name == "disabled") {
+            if ($value) {
+                $this->setAttribute("disabled", "");
+            } else {
+                $this->removeAttribute("disabled");
+            }
+        }
+        if ($name == "name") {
+            $this->setAttribute("name", $value);
+        }
+        if ($name == "type") {
+            $this->setAttribute("type", $value);
+        }
+        if ($name == "value") {
+            $this->setAttribute("value", $value);
+        }
+
+        parent::__set($name, $value);
     }
 }
