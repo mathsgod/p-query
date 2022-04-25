5d6e3748a4a67581af70070ad6fc077e66cde2f4
Raymond Chong <mathsgod@yahoo.com>
2022-04-25 11:32:31 +0800

    update

--- src/HTMLTableElement.php
+++ src/HTMLTableElement.php
@@ -117,6 +117,53 @@ class HTMLTableElement extends HTMLElement
     }
 
 
+
+    /**
+     * Returns an HTMLTableSectionElement representing the first <thead> that is a child of the element. If none is found, a new one is created and inserted in the tree immediately before the first element that is neither a <caption>, nor a <colgroup>, or as the last child if there is no such element.
+     */
+    public function createTHead(): HTMLTableSectionElement
+    {
+        if ($this->tHead) {
+            return $this->tHead;
+        }
+        $thead = $this->ownerDocument->createElement('thead');
+        $this->prependChild($thead);
+        return $thead;
+    }
+
+    /**
+     * Removes the first <thead> that is a child of the element.
+     */
+    public function deleteTHead()
+    {
+        if ($this->tHead) {
+            $this->removeChild($this->tHead);
+        }
+    }
+
+    /**
+     * Returns an HTMLTableSectionElement representing the first <tfoot> that is a child of the element. If none is found, a new one is created and inserted in the tree as the last child.
+     */
+    public function createTFoot(): HTMLTableSectionElement
+    {
+        if ($this->tFoot) {
+            return $this->tFoot;
+        }
+        $tfoot = $this->ownerDocument->createElement('tfoot');
+        $this->appendChild($tfoot);
+        return $tfoot;
+    }
+
+    /**
+     * Removes the first <tfoot> that is a child of the element.
+     */
+    public function deleteTFoot()
+    {
+        if ($tFoot = $this->tFoot) {
+            $this->removeChild($tFoot);
+        }
+    }
+
     /**
      * Returns a HTMLTableSectionElement representing a new <tbody> that is a child of the element. It is inserted in the tree after the last element that is a <tbody>, or as the last child if there is no such element.
      */
@@ -134,27 +181,34 @@ class HTMLTableElement extends HTMLElement
         return $tbody;
     }
 
-    public function createTHead(): HTMLTableSectionElement
+    /**
+     * Returns an HTMLElement representing the first <caption> that is a child of the element. If none is found, a new one is created and inserted in the tree as the first child of the <table> element.
+     */
+    public function createCaption(): HTMLElement
     {
-        if ($this->tHead) {
-            return $this->tHead;
+        if ($this->caption) {
+            return $this->caption;
         }
-        $thead = $this->ownerDocument->createElement('thead');
-        $this->prependChild($thead);
-        return $thead;
+        $caption = $this->ownerDocument->createElement("caption");
+        $this->prependChild($caption);
+        return $caption;
     }
 
-    public function createTFoot(): HTMLTableSectionElement
+    /**
+     * Removes the first <caption> that is a child of the element.
+     */
+    public function deleteCaption()
     {
-        if ($this->tFoot) {
-            return $this->tFoot;
+        if ($caption = $this->caption) {
+            $caption->remove();
         }
-        $tfoot = $this->ownerDocument->createElement('tfoot');
-        $this->appendChild($tfoot);
-        return $tfoot;
     }
 
-    public function insertRow($index = -1)
+
+    /**
+     * Returns an HTMLTableRowElement representing a new row of the table. It inserts it in the rows collection immediately before the <tr> element at the given index position. If necessary a <tbody> is created. If the index is -1, the new row is appended to the collection. If the index is smaller than -1 or greater than the number of rows in the collection, a DOMException with the value IndexSizeError is raised.
+     */
+    public function insertRow($index = -1): HTMLTableRowElement
     {
         if ($this->tBodies->length == 0) {
             $tbody = $this->createTBody();
@@ -164,32 +218,12 @@ class HTMLTableElement extends HTMLElement
         return $tbody->insertRow($index);
     }
 
-    public function deleteTHead()
-    {
-        if ($thead = $this->tHead) {
-            $thead->remove();
-        }
-    }
-
-    public function deleteCaption()
-    {
-        if ($caption = $this->caption) {
-            $caption->remove();
-        }
-    }
-
-    public function deleteTFoot()
-    {
-        if ($tfoot = $this->tfoot) {
-            $tfoot->remove();
-        }
-    }
-
+    /**
+     * Removes the row corresponding to the index given in parameter. If the index value is -1 the last row is removed; if it is smaller than -1 or greater than the amount of rows in the collection, a DOMException with the value IndexSizeError is raised.
+     */
     public function deleteRow(int $index)
     {
-        /**
-         * Removes the row corresponding to the index given in parameter. If the index value is -1 the last row is removed; if it is smaller than -1 or greater than the amount of rows in the collection, a DOMException with the value IndexSizeError is raised.
-         */
+
         if ($this->tBodies->length == 0) {
             $tbody = $this->createTBody();
         } else {
